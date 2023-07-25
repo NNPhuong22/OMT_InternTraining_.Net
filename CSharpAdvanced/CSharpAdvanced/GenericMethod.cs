a601a7d0d04a9c9715a3e4bd4a370cb80e17cab9
PhuongTheDev <108411518+PhuongTheDev@users.noreply.github.com>
2023-07-25 17:40:02 +0700

    update Generic
    Trả lời câu hỏi về indexer và làm ví dụ

--- CSharpAdvanced/CSharpAdvanced/GenericMethod.cs
+++ CSharpAdvanced/CSharpAdvanced/GenericMethod.cs
@@ -4,9 +4,14 @@
     {
         public class GenericList<T>
         {
-            public void Print(T value)
+            private T[] values;
+            public GenericList()
             {
-                Console.WriteLine(value);
+                values = new T[10];
+            }
+            public GenericList(int a)
+            {
+                values = new T[a];
             }
             public T this[int index]
             {
@@ -19,20 +24,44 @@
 
                 // khi trong hàm chưa có code và chưa return lại gì thì dùng NotImplementedException() để không bị lỗi trong compile time
                 //dunp: hàm này   public T this[int index] được gọi là gì, và dùng ntn trong C#
-                
-                get { throw new NotImplementedException(); }
+
+                /* hàm đó là một indexer, nó tương tự property, nhưng có đánh dấu chỉ mục, như là 1 mảng áo 
+                 Dùng từ khóa this
+                 */
+
+                get { return values[index]; }
+                set
+                {
+                    values[index] = value;
+                }
             }
         }
         class Program
-    {
-        static void Main(string[] args)
         {
-            GenericList<int> genericMethod = new GenericList<int>();
-            genericMethod.Print(9);
-
-
+            static void Main(string[] args)
+            {
+                GenericList<int> genericMethod = new GenericList<int>(5);
+                genericMethod[0] = 1;
+                genericMethod[1] = 2;
+                genericMethod[2] = 3;
+                genericMethod[3] = 4;
+                genericMethod[4] = 5;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(genericMethod[i]);
+                }
+                GenericList<string> indexerString = new GenericList<string>(5);
+                indexerString[0] = "a";
+                indexerString[1] = "b";
+                indexerString[2] = "c";
+                indexerString[3] = "d";
+                indexerString[4] = "e";
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(indexerString[i]);
+                }
+            }
         }
     }
-    }
-     
+
 }
--- CSharpAdvanced/Casting/Program.cs
+++ CSharpAdvanced/Casting/Program.cs
@@ -1,14 +1,57 @@
-﻿namespace Casting
+﻿namespace Generic
 {
-    class Program
+    class GenericMethod
     {
-        static void Main(string[] args)
+        public class GenericList<T>
         {
-            Text text = new Text();
-            Shape shape = text;
-            text.x = 200;
-            shape.x = 100;
-            Console.WriteLine(text.x);
+            private T[] values;
+            public GenericList()
+            {
+                values = new T[10];
+            }
+            public GenericList(int a)
+            {
+                values = new T[a];
+            }
+            public T this[int index]
+            {
+                //dunp: cái này là gì, và dùng như thế nào , trả lời = comment ở đây, và viết ví dụ dùng ở console
+
+                /* Generic dùng để tùy biến các tham số truyền vào ở các dạng khác nhau. ví dụ 1 hàm add bình thường thì với mỗi loại tham số 
+                truyền vào như string, int, double,..... thì phải viết từng hàm riêng để truyền tham số vào, làm phình code lên. Dùng generic 
+                ta có thể định nghĩa kiểu tham số truyền vào khi sử dụng.
+                 */
+
+                // khi trong hàm chưa có code và chưa return lại gì thì dùng NotImplementedException() để không bị lỗi trong compile time
+                //dunp: hàm này   public T this[int index] được gọi là gì, và dùng ntn trong C#
+
+                /* hàm đó là một indexer, nó tương tự property, nhưng có đánh dấu chỉ mục, như là 1 mảng áo 
+                 Dùng từ khóa this
+                 */
+
+                get { return values[index]; }
+                set
+                {
+                    values[index] = value;
+                }
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                GenericList<int> genericMethod = new GenericList<int>(5);
+                genericMethod[0] = 1;
+                genericMethod[1] = 2;
+                genericMethod[2] = 3;
+                genericMethod[3] = 4;
+                genericMethod[4] = 5;
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(genericMethod[i]);
+                }
+            }
         }
     }
+
 }
