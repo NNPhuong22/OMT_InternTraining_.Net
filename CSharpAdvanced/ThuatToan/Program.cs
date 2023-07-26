a714dff4386fd9f578159440a28aef120faaa142
PhuongTheDev <108411518+PhuongTheDev@users.noreply.github.com>
2023-07-26 17:15:54 +0700

    chỉnh sửa bài tập theo comment

--- CSharpAdvanced/ThuatToan/Program.cs
+++ CSharpAdvanced/ThuatToan/Program.cs
@@ -4,156 +4,182 @@ namespace ThuatToan
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            // 111
-            //   Console.WriteLine("a");
-            //   var a = Convert.ToInt32(Console.ReadLine());
-            //   for (int i = 0; i < a; i++)
-            //   {
-            //       for (int j = 0; j < a - i; j++)
-            //       {
-            //           Console.Write(" ");
-            //       }
-            //       for (int j = 0; j < 2 * i - 1; j++)
-            //       {
-            //           Console.Write("*");
-            //       }
-            //       Console.WriteLine();
-            //   }
+            //// 111
 
-            //   Console.WriteLine("b");
-            //   var b = Convert.ToInt32(Console.ReadLine());
-            //   /*
-            //                   b=5
-            //    *              i=1; j=1  * = 5
-            //   * *             i=2; j=2  * = 4 6
-            //  *   *            i=3; j=3  * = 3 7
-            // *     *           i=4; j=4  * = 2 8
-            //*********
+            //Bai111();
 
-            //    */
-            //   for (int i = 1; i <= b; i++)
-            //   {
-            //       for (int j = 1; j < 2 * b; j++)
-            //       {
-            //           if (Math.Abs(b - j) == i - 1 || i == b)
-            //           {
-            //               Console.Write("*");
-            //           }
-            //           else
-            //           {
-            //               Console.Write(" ");
-            //           }
-            //       }
-            //       Console.WriteLine();
-            //   }
+            //// 112
+
+            //Bai112();
 
 
-            ////c
-            //Console.WriteLine("c");
-            //var c = Convert.ToInt32(Console.ReadLine());
+            ////// 134
 
-            //for (int i = 1; i <= c; i++)
-            //{
-            //    for (int j = 1; j <= i; j++)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
+            //Bai134();
 
+            //////155
 
-            ////d
-            //Console.WriteLine("d");
-            //var d = Convert.ToInt32(Console.ReadLine());
+            //Bai155();
 
-            ///*
+            ////233
+            //Bai233();
 
-            //*
-            //* *
-            //*   *
-            //*     *
-            //*********
+            //// 384
 
-            // */
-            //for (int i = 1; i <= d; i++)
-            //{
-            //    for (int j = 1; j < 2 * d; j++)
-            //    {
-            //        if (j == 1 || j == i * 2 - 1)
-            //        {
-            //            Console.Write("*");
-            //        }
-            //        else if (i == d)
-            //        {
-            //            Console.Write(" ");
-            //            Console.Write("*");
-            //            j++;
-            //        }
-            //        else
-            //        {
-            //            Console.Write(" ");
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //}
+            //Bai384(b);
+
+            //// 407
+            Bai407();
+
+        }
+        //dunp: 20230726# tạo hàm riêng cho các bài 111,112 ... 449 ... rồi gọi hàm trong void main . 
 
 
+        public static void Bai111()
+        {
+            Console.WriteLine("a");
+            var a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("b");
+            var b = Convert.ToInt32(Console.ReadLine());
+            /*
+                            b=5
+             *              i=1; j=1  * = 5
+            * *             i=2; j=2  * = 4 6
+           *   *            i=3; j=3  * = 3 7
+          *     *           i=4; j=4  * = 2 8
+         *********
 
-            // 112
+             */
+            for (int i = 1; i <= b; i++)
+            {
+                for (int j = 1; j < 2 * b; j++)
+                {
+                    if (Math.Abs(b - j) == i - 1 || i == b)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
 
-            ////a
-            //int dai = Convert.ToInt32(Console.ReadLine());
-            //int rong = Convert.ToInt32(Console.ReadLine());
-            //for (int i = 1; i <= rong; i++)
-            //{
-            //    for (int j = 1; j <= dai; j++)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
+
+            //c
+            Console.WriteLine("c");
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= c; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            //d
+            Console.WriteLine("d");
+            var d = Convert.ToInt32(Console.ReadLine());
+
+            /*
+
+            *
+            * *
+            *   *
+            *     *
+            *********
+
+             */
+            for (int i = 1; i <= d; i++)
+            {
+                for (int j = 1; j < 2 * d; j++)
+                {
+                    if (j == 1 || j == i * 2 - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else if (i == d)
+                    {
+                        Console.Write(" ");
+                        Console.Write("*");
+                        j++;
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Bai112()
+        {
+            //a
+            int dai = Convert.ToInt32(Console.ReadLine());
+            int rong = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= rong; i++)
+            {
+                for (int j = 1; j <= dai; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
             //b
-            //int dai = Convert.ToInt32(Console.ReadLine());
-            //int rong = Convert.ToInt32(Console.ReadLine());
 
-            //for (int i = 1; i <= rong; i++)
-            //{
-            //    for (int j = 1; j <= dai; j++)
-            //    {
-            //        if (j == 1 || j == dai || i == 1 || i == rong)
-            //        {
-            //            Console.Write("*");
-            //        }
-            //        else
-            //        {
-            //            Console.Write(" ");
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //}
+            for (int i = 1; i <= rong; i++)
+            {
+                for (int j = 1; j <= dai; j++)
+                {
+                    if (j == 1 || j == dai || i == 1 || i == rong)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
 
+        public static void Bai134()
+        {
 
-            //// 134
-            //00:00:00.0083224
+            int[] a = { 12, 20, 1, 60 };
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine(lonnhat(a));
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
 
-            //int[] a = { 12, 20, 1, 60 };
-            //var sw = Stopwatch.StartNew();
-            //Console.WriteLine(lonnhat(a));
-            //sw.Stop();
-            //Console.WriteLine(sw.Elapsed);
-            ////00:00:00.0063116
-            //var sw2 = Stopwatch.StartNew();
-            //Console.WriteLine(a.Max(x => x));
+            var sw2 = Stopwatch.StartNew();
+            Console.WriteLine(a.Max(x => x));
 
-            //sw2.Stop();
-            //Console.WriteLine(sw2.Elapsed);
-
-
-
-            ////155
+            sw2.Stop();
+            Console.WriteLine(sw2.Elapsed);
+        }
+        public static void Bai155()
+        {
             int[] a = { 12, -50, 1, 60 };
 
             var sw = Stopwatch.StartNew();
@@ -165,29 +191,7 @@ namespace ThuatToan
             Console.WriteLine(a.MaxBy(o => Math.Abs(15 - o)));
             sw2.Stop();
             Console.WriteLine(sw2.Elapsed);
-
-            ////233
-            //int[] a = { 12, 50, 1, 60, 1, 1, 23 };
-            //Bai233(a);
-
-            //// 384
-            //int[] a = { 12, 50, 1, 60, 1, 1, 23 };
-            //List<int> b = new List<int>();
-            //for (int i = 0; i < a.Length; i++)
-            //{
-            //    while (a[i] != 0)
-            //    {
-            //        b.Add(a[i] % 10);
-            //        a[i] /= 10;
-            //    }
-            //}
-            //Bai384(b);
-
-            //// 407
-            //Bai407(381);
-
         }
-        //dunp: 20230726# tạo hàm riêng cho các bài 111,112 ... 449 ... rồi gọi hàm trong void main . 
         public static int lonnhat(int[] a)
         {
             int max = a[0];
@@ -215,37 +219,40 @@ namespace ThuatToan
             }
             return max;
         }
-        public static void Bai233(int[] arr)
-        {  
+        public static void Bai233()
+        {
             //dunp: 20230726# tìm hiểu dictionary để lưu số lần xuất hiện của 1 element cùng value trong mảng
-            int[] fr = new int[arr.Length];
-            int visited = -1;
-
-            for (int i = 0; i < arr.Length; i++)
+            int[] a = { 12, 50, 1, 60, 1, 1, 23 };
+            IDictionary<int, int> numFrequency = new Dictionary<int, int>();
+            for (int i = 0; i < a.Length; i++)
             {
-                int count = 1;
-                for (int j = i + 1; j < arr.Length; j++)
+                if (!numFrequency.ContainsKey(a[i]))
                 {
-                    if (arr[i] == arr[j])
-                    {
-                        count++;
-                        fr[j] = visited;
-                    }
+                    numFrequency.Add(a[i], 0);
                 }
-                if (fr[i] != visited)
-                    fr[i] = count;
+                numFrequency[a[i]]++;
             }
-
-            Console.WriteLine(" gia tri  tan suat");
-            for (int i = 0; i < fr.Length; i++)
+            foreach (KeyValuePair<int, int> kv in numFrequency)
             {
-                if (fr[i] != visited)
-                    Console.WriteLine("    " + arr[i] + "         " + fr[i]);
+                Console.WriteLine(kv.Key + " - " + kv.Value);
             }
         }
         public static void Bai384(List<int> arr)
         {
-          //dunp: 20230726# xem lại bài chưa thấy logic xác định từng chữ số trong số lớn hơn 1 chữ số . 
+            //dunp: 20230726# xem lại bài chưa thấy logic xác định từng chữ số trong số lớn hơn 1 chữ số . 
+            int[] a = { 12, 50, 1, 60, 1, 1, 23 };
+            List<int> b = new List<int>();
+
+            // em dùng vòng for với while này để tách từng chữ số ở các số rồi cho vào list b ở bên trên ạ
+            for (int i = 0; i < a.Length; i++)
+            {
+                while (a[i] != 0)
+                {
+                    b.Add(a[i] % 10);
+                    a[i] /= 10;
+                }
+            }
+
             int[] fr = new int[arr.Count];
             int visited = -1;
             int max = 0;
@@ -274,30 +281,57 @@ namespace ThuatToan
             }
             Console.WriteLine("Chu so xuat hien nhieu nhat: " + number + " so lan suat hien: " + max);
         }
-        public static void Bai407(double number)
+        public static void Bai407()
         {
-            ////dunp: 20230726# ý bài này là cho 1 ma trận: m*n, n là số dòng, và viết thuật toán để sắp xếp tăng dần cho từng dòng từ trái sang 
-            List<int> arr = new List<int>();
-            while (Convert.ToInt32(number) != 0)
+            ////dunp: 20230726# ý bài này là cho 1 ma trận: m*n, n là số dòng, và viết thuật
+            //toán để sắp xếp tăng dần cho từng dòng từ trái sang 
+            int hang = 0;
+            int cot = 0;
+
+            Console.Write("So Hang:");
+            hang = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("So cot:");
+            cot = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[hang, cot];
+
+
+            for (int i = 0; i < hang; i++)
             {
-                arr.Add((int)(number % 10));
-                number /= 10;
-            }
-            for (int i = 0; i < arr.Count; i++)
-            {
-                for (int j = i + 1; j < arr.Count; j++)
+
+                for (int j = 0; j < cot; j++)
                 {
-                    if (arr[i] > arr[j])
-                    {
-                        int swap = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = swap;
-                    }
+                    Console.Write("arr[{0}][{1}]:", i, j);
+                    arr[i, j] = int.Parse(Console.ReadLine());
                 }
             }
-            for (int i = 0; i < arr.Count; i++)
+
+            for (int i = 0; i < hang; i++)
             {
-                Console.Write(arr[i]);
+
+                for (int n = 0; n < cot; n++)
+                {
+                    for (int k = n + 1; k < cot; k++)
+                    {
+                        if (arr[i, n] > arr[i, k])
+                        {
+                            int swap = arr[i, n];
+                            arr[i, n] = arr[i, k];
+                            arr[i, k] = swap;
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < hang; i++)
+            {
+                for (int j = 0; j < cot; j++)
+                {
+                    Console.Write(" " + arr[i, j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
---
+++ Ex2_OMT/Ex2_OMT/Repositories/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ex2_OMT.Models;
+
+namespace Ex2_OMT.Repositories
+{
+    public interface IRoleRepository
+    {
+        Task<ApiResponse<Role>> AddRole();
+        Task<ApiResponse<Role>> ChangeUserRole(int id);
+    }
+}
