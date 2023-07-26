7c3aa93869df03a8edb948e9919a8d670776b969
PhuongTheDev <108411518+PhuongTheDev@users.noreply.github.com>
2023-07-26 14:42:42 +0700

    hoàn thành bài tập  155, 233, 384, 407

--- CSharpAdvanced/ThuatToan/Program.cs
+++ CSharpAdvanced/ThuatToan/Program.cs
@@ -134,15 +134,44 @@
             //}
 
 
-            // 134
-            float[] a = { 12, 20, 1, 60 };
-            Program ab = new Program();
-            Console.WriteLine(lonnhat(a));
+            //// 134
+            //int[] a = { 12, 20, 1, 60 };
+            //Console.WriteLine(lonnhat(a));
+
+
+
+            ////155
+            //int[] a = { 12, -50, 1, 60 };
+            //Console.WriteLine(Bai155(a, 15));
+
+            ////233
+            //int[] a = { 12, 50, 1, 60, 1, 1, 23 };
+            //Bai233(a);
+
+            //// 384
+            //int[] a = { 12, 50, 1, 60, 1, 1, 23 };
+            //List<int> b = new List<int>();
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    while (a[i] != 0)
+            //    {
+            //        b.Add(a[i] % 10);
+            //        a[i] /= 10;
+            //    }
+            //}
+            //Bai384(b);
+
+            // 407
+            Bai407(381);
+
+
+            // 449
+
 
         }
-        public static float lonnhat(float[] a)
+        public static int lonnhat(int[] a)
         {
-            float max = a[0];
+            int max = a[0];
             for (int i = 1; i < a.Length; i++)
             {
                 if (a[i] > max)
@@ -151,6 +180,103 @@
                 }
             }
             return max;
+        }
+        public static int Bai155(int[] a, int num)
+        {
+            int max = 0;
+            int com = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Math.Abs(a[i] - num) > com)
+                {
+                    com = Math.Abs(a[i] - num);
+                    max = a[i];
+
+                }
+            }
+            return max;
+        }
+        public static void Bai233(int[] arr)
+        {
+            int[] fr = new int[arr.Length];
+            int visited = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        fr[j] = visited;
+                    }
+                }
+                if (fr[i] != visited)
+                    fr[i] = count;
+            }
+
+            Console.WriteLine(" gia tri  tan suat");
+            for (int i = 0; i < fr.Length; i++)
+            {
+                if (fr[i] != visited)
+                    Console.WriteLine("    " + arr[i] + "         " + fr[i]);
+            }
+        }
+        public static void Bai384(List<int> arr)
+        {
+            int[] fr = new int[arr.Count];
+            int visited = -1;
+            int max = 0;
+            int number = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int count = 1;
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        fr[j] = visited;
+                    }
+                }
+                if (fr[i] != visited)
+                    fr[i] = count;
+            }
+            for (int i = 0; i < fr.Length; i++)
+            {
+                if (fr[i] > max)
+                {
+                    max = fr[i];
+                    number = arr[i];
+                }
+            }
+            Console.WriteLine("Chu so xuat hien nhieu nhat: " + number + " so lan suat hien: " + max);
+        }
+        public static void Bai407(double number)
+        {
+            List<int> arr = new List<int>();
+            while (Convert.ToInt32(number) != 0)
+            {
+                arr.Add((int)(number % 10));
+                number /= 10;
+            }
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int swap = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swap;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.Write(arr[i]);
+            }
         }
     }
 }
