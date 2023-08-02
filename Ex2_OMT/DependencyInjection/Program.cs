9372d6022662442c3b46e3d4a7fe973eb88fc46b
PhuongTheDev <108411518+PhuongTheDev@users.noreply.github.com>
2023-08-02 10:53:17 +0700

    Thêm code dependency injection

---
+++ Ex2_OMT/DependencyInjection/Program.cs
@@ -0,0 +1,28 @@
+using DependencyInjection.Interface;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ITransientService, SomeService>();
+builder.Services.AddScoped<IScopedService, SomeService>();
+builder.Services.AddSingleton<ISingletonService, SomeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
